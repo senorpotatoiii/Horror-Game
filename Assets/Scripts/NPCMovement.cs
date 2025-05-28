dd07f46a1bd21c470eca2caeb4969879320a3b41
zns.sweet <zns.sweet@yahoo.com>
2025-05-27 22:59:51 -0400

    did uh movement stuff
    yup

--- Assets/Scripts/NPCMovement.cs
+++ Assets/Scripts/NPCMovement.cs
@@ -2,19 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCMovement : MonoBehaviour
-{
+public class NPCMovement : MonoBehaviour{
     Rigidbody2D rb;
-    
+    [SerializeField] float timeBetweenMove = 2f;
+    [SerializeField] float varienceTimeMove = 0.2f;
+    [SerializeField] float minTimeMove = 1f;
+    [SerializeField] bool moving = false;
+    [SerializeField] Vector2 currentSpeed;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
+        currentSpeed = new Vector2(0f, 0f);
         rb = GetComponent<Rigidbody2D>();
     }
-    
+
     public void TestMove()
     {
-        Debug.Log("Test");
-        // Call MoveTo method.
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            currentSpeed.x = Random.Range(-1.0f, 1.0f) * 2;
+            currentSpeed.y = Random.Range(-1.0f, 1.0f) * 2;
+            moving = true;
+            rb.velocity = currentSpeed;
+            moving = false;
+            yield return new WaitForSeconds(timeBetweenMove);
+        }
+        Debug.Log("Test NPC Move");
     }
 }
